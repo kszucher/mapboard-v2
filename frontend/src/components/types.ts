fafcc88b566008f47adec60be408e5fb3ae8bb4c
krisz <krisztian@szucher.com>
2026-06-25 18:38:47 +0200

    cleanup

--- frontend/src/components/types.ts
+++ frontend/src/components/types.ts
@@ -1,5 +1,4 @@
 import type { Edge, Node } from '@xyflow/react';
-import type { ElkEdgeSection } from "elkjs";
 import type { components } from '../api/generated/schema';
 
 export type ApiNode = components['schemas']['NodeRead'];
@@ -7,4 +6,4 @@ export type ApiEdge = components['schemas']['EdgeRead'];
 export type ApiExpression = components['schemas']['ExpressionRead'];
 
 export type AppFlowNode = Node<{ node: ApiNode; layer?: number; expressions?: ApiExpression[] }, 'custom'>;
-export type AppFlowEdge = Edge<{ edge?: ApiEdge; sections?: ElkEdgeSection[] }, 'custom'>;
+export type AppFlowEdge = Edge<{ edge?: ApiEdge }, 'custom'>;
